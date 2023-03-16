90d13b2379cc473ef414907975f9e3431dd7e33e
Kacper Kaczmarczyk <kacperkaczmarczyktic@gmail.com>
2023-03-16 10:00:34 +0100

    JUnit added. First Unit test added.

---
+++ src/main/java/org/cpm/ActivityFlowListTest.java
@@ -0,0 +1,47 @@
+package org.cpm;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActivityFlowListTest {
+
+
+
+    @Test
+    void logic() {
+        List<Activity> activities = new ArrayList<Activity>();
+
+        activities.add(new Activity("A", "opracowanie zalozen konstrukcyjnych", "-", 5));
+        activities.add(new Activity("B", "analiza propozycji uruchomienia nowej produkcji", "-", 7));
+        activities.add(new Activity("C", "sporzadzenie projektow technicznych podzespolow", "A", 6));
+        activities.add(new Activity("D", "zamowienie materialow", "A", 8));
+        activities.add(new Activity("E", "analiza popytu", "B", 3));
+        activities.add(new Activity("F", "budowa prototypu", "C", 4));
+        activities.add(new Activity("G", "sporzadzenie dokumentacji", "C", 2));
+        activities.add(new Activity("H", "pierwsza partia produkcji seryjnej", "E,D,F", 5));
+
+        ActivitesList activitiesUser = new ActivitesList(activities);
+
+        MatrixOfPredecessors matrixOfPredecessors = new MatrixOfPredecessors(activitiesUser.getActivities());
+
+        ActivityFlowList activityFlowList  = new ActivityFlowList();
+        activityFlowList.logic(activitiesUser, matrixOfPredecessors);
+
+        List<ActivityFlow> activityFlowListResultExpected = new ArrayList<>();
+
+        activityFlowListResultExpected.add(new ActivityFlow(activities.get(0), new Event(1), new Event(2)));
+        activityFlowListResultExpected.add(new ActivityFlow(activities.get(1), new Event(1), new Event(3)));
+        activityFlowListResultExpected.add(new ActivityFlow(activities.get(2), new Event(2), new Event(4)));
+        activityFlowListResultExpected.add(new ActivityFlow(activities.get(3), new Event(2), new Event(5)));
+        activityFlowListResultExpected.add(new ActivityFlow(activities.get(4), new Event(3), new Event(5)));
+        activityFlowListResultExpected.add(new ActivityFlow(activities.get(5), new Event(4), new Event(5)));
+        activityFlowListResultExpected.add(new ActivityFlow(activities.get(6), new Event(4), new Event(6)));
+        activityFlowListResultExpected.add(new ActivityFlow(activities.get(7), new Event(5), new Event(6)));
+
+        assertEquals(activityFlowListResultExpected, activityFlowList.getActivityFlowList());
+    }
+}
